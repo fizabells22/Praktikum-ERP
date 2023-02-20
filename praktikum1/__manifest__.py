b48682ec4cd964bb169fc23ff26575b287d615c9
fizabells22 <fizabella.ru@gmail.com>
2023-02-20 10:52:29 +0700

    Praktikum 1 ERP

---
+++ praktikum1/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "author"        : "Fiza Bella Rahmadanty Utomo",
+    "website"       : "http://siakad.polinema.ac.id/",
+    "category"      : "New Module",
+    "description"   : """
+        Latihan membuat addons baru di odoo 1
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [
+
+    ],
+    "demo"          :[],
+    "test"          :[],
+    "image"         :[],
+    "qweb"          :[],
+    "css"           :[],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+}
